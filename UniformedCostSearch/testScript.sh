5b0e5715c06f953772082e7d8eab4348cbb06047
Vishal Kuamr <Vishalkumartcd@gmail.com>
2020-03-31 13:34:12 +0100

    Adding UCS code and results
    Adding Uniform Cost search and results

---
+++ UniformedCostSearch/testScript.sh
@@ -0,0 +1,5 @@
+rm -f Results*
+python3 TaskExecution.py ucs 1 1,2,5,3,4,0,6,7,8
+python3 TaskExecution.py ucs 2 3,1,2,0,4,5,6,7,8
+python3 TaskExecution.py ucs 3 3,1,2,4,5,0,6,7,8
+python3 TaskExecution.py ucs 4 1,2,5,0,3,4,6,7,8
